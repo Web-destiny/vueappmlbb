625c0b7955a29bb2dfbcea7f203a9ec5404e17c9
Kuroro <takehiko.suzume@yandex.ru>
2024-02-07 12:29:28 +0300

    правки

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <my-component></my-component>
-  <home-page></home-page>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
-
-<script>
-
-import MyComponent from "@/MyComponent";
-import HomePage from "@/components/homePage";
-
-export default {
-  name: 'App',
-  components: {HomePage, MyComponent },
-}
-
-</script>
 
 <style>
 #app {
@@ -23,6 +13,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
