d25929e2cdfbd1bf29ca8c64ff3c9ac0b3c37d36
Kuroro <takehiko.suzume@yandex.ru>
2024-02-17 16:27:59 +0300

    чтение папок/получение инф. node.js

--- backend_test/index.js
+++ backend_test/index.js
@@ -1,3 +1,55 @@
 const { randInt, randArr } = require('./random')
 
+const fs = require('fs')
+const path = require('path')
+
 console.log( randInt(), randArr())
+
+function f01(){
+    const pathName = 'random'
+    console.log(fs.existsSync(pathName))
+}
+f01();
+
+function f02(){
+    const pathName = 'random/rand_arr.js'
+    const fileInfo = fs.statSync(pathName)
+    console.log(fileInfo.size)
+}
+f02();
+
+function f03(){
+    const pathName = 'random/rand_arr.js'
+    console.log(path.basename(pathName))
+    console.log(path.dirname(pathName))
+    console.log(path.extname(pathName))
+    console.log(path.parse(pathName))
+}
+
+f03();
+
+function f04(){
+    const pathName = 'random'
+    console.log(fs.readdirSync(pathName))
+}
+
+f04();
+
+function f05(){
+    const pathName = 'random'
+    const dirArr = fs.readdirSync(pathName)
+    let out = ''
+    dirArr.forEach(item => out += item + '\n')
+    console.log(out)
+}
+
+f05();
+
+function f06(){
+    const pathName = 'random/rand_int.js'
+    // const dirArr = fs.readdirSync(pathName)
+    let absolutePath = path.join(__dirname, pathName)
+    console.log(absolutePath)
+}
+
+f06();
