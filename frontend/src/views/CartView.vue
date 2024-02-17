384db254a8101a49129be5d4c09a100a019dea58
Kuroro <takehiko.suzume@yandex.ru>
2024-02-17 23:42:58 +0300

    стили/верстка для корзины

--- frontend/src/views/CartView.vue
+++ frontend/src/views/CartView.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="cart">
-    <h2>Корзина</h2>
-    <ul>
-      <li v-for="item in cartItems" :key="item.id">
-        {{ item.name }} - {{ formattedPrice(item.price) }} руб.
-        <button @click="removeFromCart(item.id)">Удалить из корзины</button>
+    <div class="cart-title">Ваша корзина</div>
+    <ul class="products-list">
+      <li v-for="item in cartItems"
+          :key="item.id"
+          class="product-item col-6 col-lg-3"
+      >
+        <div class="product-item-content">
+          <div class="product-item__text">{{ item.name }} - <strong>{{ formattedPrice(item.price) }} руб.</strong></div>
+          <div class="product-item__image">
+            <img :src="item.image_url" alt="Product Image">
+          </div>
+        </div>
+        <button
+            @click="removeFromCart(item.id)"
+            class="btn btn-primary"
+        >
+          Удалить из корзины
+        </button>
       </li>
     </ul>
-    <p v-if="cartItems.length === 0">Корзина пуста</p>
-    <button @click="clearCart">Очистить корзину</button>
+    <p
+        v-if="cartItems.length === 0"
+    >
+      Корзина пуста
+    </p>
+    <button
+        @click="clearCart"
+        class="btn btn-success btn-clear-cart"
+    >
+      Очистить корзину
+    </button>
   </div>
 </template>
 
@@ -35,5 +57,58 @@ export default {
 </script>
 
 <style scoped>
-/* Стили для компонента корзины */
+
+.products-list {
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  grid-gap: 10px;
+  margin-top: 20px;
+  padding-left: 0;
+}
+
+.product-item {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  gap: 10px;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  background-color: #ecf1ff;
+  padding-left: 20px;
+}
+
+.product-item-content {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+
+.product-item .product-item__image {
+  width: auto;
+  max-height: 60px;
+  text-align: center;
+  position: relative;
+}
+.product-item .product-item__image img {
+  margin: 0 auto;
+  vertical-align: middle;
+  border-style: none;
+  max-width: 100%;
+  min-height: 60px;
+  max-height: 60px;
+  padding: 5px 0px;
+}
+
+.cart-title {
+  text-align: left;
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.btn-clear-cart {
+  margin-top: 20px;
+}
+
 </style>
